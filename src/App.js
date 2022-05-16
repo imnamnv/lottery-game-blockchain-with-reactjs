d82903717216466e951d7862422a42a321de9691
NamNguyen <imnamnv@gmail.com>
2022-05-16 16:49:37 +0700

    lottery game blockchain with ReactJs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import web3 from "./web3";
+import { useEffect, useState } from "react";
+import lottery from "./lottery";
 
 function App() {
+  const [manager, setManager] = useState("");
+  const [playerList, setPlayerList] = useState([]);
+  const [balance, setBalance] = useState("");
+  const [value, setValue] = useState("");
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      // don't need call({from: xxx}). Only in call, send is not
+      //because we use provider of Metamask, it have default from address
+      setManager(await lottery.methods.manager().call());
+      setPlayerList(await lottery.methods.getPlayerList().call());
+      setBalance(await web3.eth.getBalance(lottery.options.address));
+    })();
+  }, []);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const accounstList = await web3.eth.getAccounts();
+    setMessage("Loading...");
+
+    // not like call, we need sent address who send create this transaction
+    await lottery.methods.enter().send({
+      from: accounstList[0],
+      value: web3.utils.toWei(value, "ether"),
+      gas: 1000000,
+    });
+
+    setMessage("You entered!!!");
+  };
+
+  const onClick = async () => {
+    const accounstList = await web3.eth.getAccounts();
+
+    setMessage("Pick a winner processing....");
+    await lottery.methods.pickWinner().send({
+      gas: 1000000,
+      from: accounstList[0],
+    });
+
+    setMessage("A winner has been picked!!!");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Manager: {manager}</div>
+      <div>Player number: {playerList.length}</div>
+      <div>Balance win: {web3.utils.fromWei(balance, "ether")}</div>
+      <hr />
+      <form onSubmit={onSubmit}>
+        <h4>Want to try your luck?</h4>
+        <label>
+          Amount(Must {">"} 0.011)
+          <input
+            value={value}
+            onChange={(event) => {
+              setValue(event.target.value);
+            }}
+          />
+        </label>
+        <button>Enter</button>
+      </form>
+      <hr />
+      <h4>Pick a winner</h4>
+      <button onClick={onClick}>Pick</button>
+      <hr />
+      <h2>{message}</h2>
     </div>
   );
 }
---
+++ src/lottery.js
@@ -0,0 +1,59 @@
+import web3 from "./web3";
+
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayerList",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "playerList",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+];
+
+const address = "0x346Fc09427327873d3D5C5e86B1136b7E834908e";
+
+export default new web3.eth.Contract(abi, address);
